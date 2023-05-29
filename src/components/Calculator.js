2903cf006d681c7bf2155bbc9fa6117d951362de
Sergio Peralta <seperalta88@gmail.com>
2023-05-29 14:41:24 -0300

    Add styles

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,32 +6,32 @@ function Calculator() {
         <button type="button">AC</button>
         <button type="button">+/-</button>
         <button type="button">%</button>
-        <button type="button" className="orange">
+        <button type="button" id="orange">
           ÷
         </button>
         <button type="button">7</button>
         <button type="button">8</button>
         <button type="button">9</button>
-        <button type="button" className="orange">
+        <button type="button" id="orange">
           x
         </button>
         <button type="button">4</button>
         <button type="button">5</button>
         <button type="button">6</button>
-        <button type="button" className="orange">
+        <button type="button" id="orange">
           -
         </button>
         <button type="button">1</button>
         <button type="button">2</button>
         <button type="button">3</button>
-        <button type="button" className="orange">
+        <button type="button" id="orange">
           +
         </button>
         <button type="button" className="btn0">
           0
         </button>
         <button type="button">.</button>
-        <button type="button" className="orange">
+        <button type="button" id="orange">
           =
         </button>
       </div>
