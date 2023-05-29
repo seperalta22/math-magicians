b7a9a807ba1a3e2465fdd386910d9a7519750837
Sergio Peralta <seperalta88@gmail.com>
2023-05-29 17:33:59 -0300

    Refactor calculate.js file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,22 +1,22 @@
-import buttons from '../logic/buttons';
+import buttons from '../logic';
 
 function Calculator() {
-  return (
-    <div className="calculator">
-      <input type="text" className="calculator-display" readOnly />
-      <div className="calculator-buttons">
-        {buttons.map((button) => (
-          <button
-            type="button"
-            key={button.name}
-            className={`${button.className} ${button.color}`}
-          >
-            {button.name}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className='calculator'>
+			<input type='text' className='calculator-display' readOnly />
+			<div className='calculator-buttons'>
+				{buttons.map((button) => (
+					<button
+						type='button'
+						key={button.name}
+						className={`${button.className} ${button.color}`}
+					>
+						{button.name}
+					</button>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Calculator;
