1c1f17a31d29fe1d086abc8631c18c74c665d1cb
Sergio Peralta <seperalta88@gmail.com>
2023-05-29 17:47:43 -0300

    Implement calculator logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,22 +1,43 @@
-import buttons from '../logic';
+import { useState } from 'react';
+import buttons from '../logic/buttons';
+import calculate from '../logic/calculate';
 
 function Calculator() {
-	return (
-		<div className='calculator'>
-			<input type='text' className='calculator-display' readOnly />
-			<div className='calculator-buttons'>
-				{buttons.map((button) => (
-					<button
-						type='button'
-						key={button.name}
-						className={`${button.className} ${button.color}`}
-					>
-						{button.name}
-					</button>
-				))}
-			</div>
-		</div>
-	);
+  const [calculation, setCalculation] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (e) => {
+    setCalculation(calculate(calculation, e.target.textContent));
+  };
+
+  const { total, next } = calculation;
+  const display = next || total || '0';
+
+  return (
+    <div className="calculator">
+      <input
+        type="text"
+        className="calculator-display"
+        value={display}
+        readOnly
+      />
+      <div className="calculator-buttons">
+        {buttons.map((button) => (
+          <button
+            type="button"
+            key={button.name}
+            className={`${button.className} ${button.color}`}
+            onClick={handleClick}
+          >
+            {button.name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Calculator;
