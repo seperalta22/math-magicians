8f5ab0a60919127f5a15cadc1e7beaab6bf3864e
Sergio Peralta <seperalta88@gmail.com>
2023-05-29 17:33:03 -0300

    Refactor calculate.js file

---
+++ src/logic/buttons.js
@@ -0,0 +1,23 @@
+const buttons = [
+  { name: 'AC' },
+  { name: '+/-' },
+  { name: '%' },
+  { name: '÷', color: 'orange' },
+  { name: '7' },
+  { name: '8' },
+  { name: '9' },
+  { name: 'x', color: 'orange' },
+  { name: '4' },
+  { name: '5' },
+  { name: '6' },
+  { name: '-', color: 'orange' },
+  { name: '1' },
+  { name: '2' },
+  { name: '3' },
+  { name: '+', color: 'orange' },
+  { name: '0', className: 'btn0' },
+  { name: '.' },
+  { name: '=', color: 'orange' },
+];
+
+export default buttons;
