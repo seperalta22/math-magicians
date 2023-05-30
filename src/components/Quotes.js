7a035615aa4644c07cf39eba2601cb3a016a6c49
Sergio Peralta <seperalta88@gmail.com>
2023-05-29 22:47:25 -0300

    Implement quotes functionality

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,7 +1,55 @@
+import { useEffect, useState } from 'react';
+
 function Quotes() {
+  const [quotes, setQuotes] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  // usually I would put this in a .env file but I'm not sure
+  // if the reviewer will be able to see it.
+  const API_ID = 'dByEAwV9LJFlCKUgjAX60A==fvZH8LP91QrzRc6I';
+  const URL = 'https://api.api-ninjas.com/v1/quotes';
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const response = await fetch(URL, {
+          headers: {
+            'X-Api-Key': API_ID,
+          },
+        });
+        const data = await response.json();
+        setQuotes(data[0]);
+        setLoading(false);
+      } catch (error) {
+        setError('Error fetching quote');
+        setLoading(false);
+      }
+    };
+    fetchQuote();
+  }, []);
+
   return (
-    <div className="quotes">
-      <h1>Quotes</h1>
+    <div className="quotes-container">
+      {loading && <div className="loading">Loading...</div>}
+      {error && (
+      <div className="error">
+        {error}
+        - Please try again later
+      </div>
+      )}
+      {quotes && (
+      <p className="quote">
+        &quot;
+        {quotes.quote}
+        {' '}
+        &quot; -
+        <span>
+          {' '}
+          {quotes.author}
+        </span>
+      </p>
+      )}
     </div>
   );
 }
