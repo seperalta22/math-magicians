3e3293e3a16feb754324451713f14ee77ccdff30
Sergio Peralta <seperalta88@gmail.com>
2023-06-05 19:05:43 -0300

    Add styles to pages

--- src/pages/CalculatorPage.jsx
+++ src/pages/CalculatorPage.jsx
@@ -1,10 +1,13 @@
 import Calculator from '../components/Calculator';
+import styles from '../styles/CalculatorPage.module.css';
 
 const CalculatorPage = () => (
-  <div>
-    <h2>Let&apos;s do some math!</h2>
-    <Calculator />
-  </div>
+  <>
+    <div className={styles.container}>
+      <h2>Let&apos;s do some math!</h2>
+      <Calculator />
+    </div>
+  </>
 );
 
 export default CalculatorPage;
