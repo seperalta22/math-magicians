80e6f0761c863b2d863b3c78ca977186fe1834ac
Sergio Peralta <seperalta88@gmail.com>
2023-06-05 17:35:36 -0300

    Update quotes page content

--- src/pages/QuotePage.jsx
+++ src/pages/QuotePage.jsx
@@ -1,2 +1,8 @@
-const QuotePage = () => <div>QuotePage</div>;
+import Quotes from '../components/Quotes';
+
+const QuotePage = () => (
+  <div>
+    <Quotes />
+  </div>
+);
 export default QuotePage;
