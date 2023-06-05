5b45e54ac44a5c9768e661ff894935026e3577f2
Sergio Peralta <seperalta88@gmail.com>
2023-06-05 17:34:18 -0300

    Update calculator page content

--- src/pages/CalculatorPage.jsx
+++ src/pages/CalculatorPage.jsx
@@ -1,2 +1,10 @@
-const CalculatorPage = () => <div>CalculatorPage</div>;
+import Calculator from '../components/Calculator';
+
+const CalculatorPage = () => (
+  <div>
+    <h2>Let&apos;s do some math!</h2>
+    <Calculator />
+  </div>
+);
+
 export default CalculatorPage;
