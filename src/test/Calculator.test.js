a1a3bdde9f8972267b97f619d79cf12287586130
Sergio Peralta <seperalta88@gmail.com>
2023-06-06 15:50:21 -0300

    Add logic files tests

--- src/test/Calculator.test.js
+++ src/test/Calculator.test.js
@@ -4,75 +4,75 @@ import '@testing-library/jest-dom';
 import Calculator from '../components/Calculator';
 
 describe('test snapshop', () => {
-	it('should render Calculator', () => {
-		const renderCalculator = renderer.create(<Calculator />).toJSON();
-		expect(renderCalculator).toMatchSnapshot();
-	});
+  it('should render Calculator', () => {
+    const renderCalculator = renderer.create(<Calculator />).toJSON();
+    expect(renderCalculator).toMatchSnapshot();
+  });
 });
 
 describe('test with testing library', () => {
-	test('should work for addition', () => {
-		render(<Calculator />);
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('+'));
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('='));
-		expect(screen.getByRole('textbox')).toHaveValue('16');
-	});
+  test('should work for addition', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByRole('textbox')).toHaveValue('16');
+  });
 
-	test('should work for subtraction', () => {
-		render(<Calculator />);
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('-'));
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('='));
-		expect(screen.getByRole('textbox')).toHaveValue('0');
-	});
+  test('should work for subtraction', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('-'));
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByRole('textbox')).toHaveValue('0');
+  });
 
-	test('should work for multiplication', () => {
-		render(<Calculator />);
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('x'));
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('='));
-		expect(screen.getByRole('textbox')).toHaveValue('64');
-	});
+  test('should work for multiplication', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('x'));
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByRole('textbox')).toHaveValue('64');
+  });
 
-	test('should work for division', () => {
-		render(<Calculator />);
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('÷'));
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('='));
-		expect(screen.getByRole('textbox')).toHaveValue('1');
-	});
+  test('should work for division', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('÷'));
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByRole('textbox')).toHaveValue('1');
+  });
 
-	test('should work for clear', () => {
-		render(<Calculator />);
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('+'));
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('AC'));
-		expect(screen.getByRole('textbox')).toHaveValue('0');
-	});
+  test('should work for clear', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('AC'));
+    expect(screen.getByRole('textbox')).toHaveValue('0');
+  });
 
-	test('should work for decimal', () => {
-		render(<Calculator />);
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('.'));
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('+'));
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('.'));
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('='));
-		expect(screen.getByRole('textbox')).toHaveValue('17.6');
-	});
+  test('should work for decimal', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('.'));
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('.'));
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByRole('textbox')).toHaveValue('17.6');
+  });
 
-	test('should work for negative', () => {
-		render(<Calculator />);
-		fireEvent.click(screen.getByText('8'));
-		fireEvent.click(screen.getByText('+/-'));
-		expect(screen.getByRole('textbox')).toHaveValue('-8');
-	});
+  test('should work for negative', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('+/-'));
+    expect(screen.getByRole('textbox')).toHaveValue('-8');
+  });
 });
--- src/test/logic/calculate.test.js
+++ src/test/logic/calculate.test.js
@@ -1,39 +1,38 @@
-import operate from '../../logic/operate';
-import { render, screen } from '@testing-library/react';
+import calculate from '../../logic/calculate';
 import '@testing-library/jest-dom/extend-expect';
 
-describe('operate', () => {
-	test('should perform addition correctly', () => {
-		expect(operate('2', '3', '+')).toBe('5');
-	});
+describe('calculate', () => {
+  test('should clear calculator data when AC button is pressed', () => {
+    const result = calculate(
+      {
+        total: '10',
+        next: '5',
+        operation: '+',
+      },
+      'AC',
+    );
 
-	test('should perform subtraction correctly', () => {
-		expect(operate('5', '3', '-')).toBe('2');
-	});
+    expect(result).toEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
 
-	test('should perform multiplication correctly', () => {
-		expect(operate('2', '4', 'x')).toBe('8');
-	});
+  test('should update next number when a number button is pressed', () => {
+    const result = calculate(
+      {
+        total: '10',
+        next: '5',
+        operation: '+',
+      },
+      '7',
+    );
 
-	test('should perform division correctly', () => {
-		expect(operate('10', '2', '÷')).toBe('5');
-	});
-
-	test('should return an error message when dividing by 0', () => {
-		expect(operate('5', '0', '÷')).toBe("Can't divide by 0.");
-	});
-
-	test('should perform modulo calculation correctly', () => {
-		expect(operate('7', '4', '%')).toBe('3');
-	});
-
-	test('should return an error message when finding modulo by 0', () => {
-		expect(operate('5', '0', '%')).toBe(
-			"Can't find modulo as can't divide by 0."
-		);
-	});
-
-	test('should throw an error for unknown operations', () => {
-		expect(() => operate('2', '3', '#')).toThrow("Unknown operation '#'");
-	});
+    expect(result).toEqual({
+      total: '10',
+      next: '57',
+      operation: '+',
+    });
+  });
 });
