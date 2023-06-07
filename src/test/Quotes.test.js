254bbb377058821ab055e4b3fb535119db337862
Sergio Peralta <seperalta88@gmail.com>
2023-06-07 15:40:54 -0300

    Create more Quotes component tests

--- src/test/Quotes.test.js
+++ src/test/Quotes.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import Quotes from '../components/Quotes';
 
 describe('Quotes component', () => {
@@ -12,5 +12,29 @@ describe('Quotes component', () => {
     const { getByText } = render(<Quotes />);
     const loading = getByText(/loading/i);
     expect(loading).toBeInTheDocument();
+  });
+
+  test('Renders error message if there is an error fetching quote', async () => {
+    // Mock the fetch function to simulate an error
+    global.fetch = jest.fn(() => Promise.reject(new Error('Error fetching quote')));
+
+    const { getByText } = render(<Quotes />);
+    await waitFor(() => expect(getByText(/Error fetching quote/)).toBeInTheDocument());
+  });
+
+  test('Renders fetched quote when available', async () => {
+    // Mock the fetch function to return a fake quote
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve([
+        {
+          quote: 'Test Quote',
+          author: 'Test Author',
+        },
+      ]),
+    }));
+
+    const { getByText } = render(<Quotes />);
+    await waitFor(() => expect(getByText(/Test Quote/)).toBeInTheDocument());
+    expect(getByText(/Test Author/)).toBeInTheDocument();
   });
 });
